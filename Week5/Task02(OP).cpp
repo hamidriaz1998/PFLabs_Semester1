e4d047dac809cb6d617f0adf7a1ba52d44ecdad5
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-10-03 09:50:01 +0100

    Complete Week 5, Task02(OP)

---
+++ Week5/Task02(OP).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+main()
+{
+
+	float base, exp, result;
+	cout << "Enter the base number: ";
+	cin >> base;
+	cout << "Enter the exponent: ";
+	cin >> exp;
+	result = pow(base, exp);
+	cout << base << " raised to the power " << exp << " is: ";
+}
