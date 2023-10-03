6b2b2179bd44ba1548cf628f8599c7c2d9834ba6
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-10-03 10:59:31 +0100

    Start Task6(CP)

---
+++ Week5/Task06(CP).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+string checkAlphabetCase(char);
+main()
+{
+
+    char c;
+    string answer;
+    cout << "Enter a character (A/a): ";
+    cin >> c;
+    answer = checkAlphabetCase(c);
+    cout << answer;
+}
+
+string checkAlphabetCase(char character){
+
+    if (character == 'A'){
+        return "You have entered Capital A";
+    }
+    if (character == 'a'){
+        return "You have entered small A";
+    }
+}
