16ca71d9dca84931e3cd913f0739fea67908ff09
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-10-03 09:54:37 +0100

    Complete Week 5, Task03(OP)

---
+++ Week5/Task03(OP).cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+main()
+{
+
+	float n, sq;
+	cout << "Enter a number: ";
+	cin >> n;
+	cout << "The square root of " << n << " is: " << sqrt(n);
+}
