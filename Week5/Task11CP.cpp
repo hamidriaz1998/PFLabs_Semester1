563d23ebe09519ec5faa17f9b8efc011b4f092d5
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-10-04 21:46:42 +0500

    Complete Task11CP

--- Week5/Task11CP.cpp
+++ Week5/Task11CP.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 string calculatePoolState(int, int, int, float);
 main()
@@ -17,4 +18,28 @@ main()
     cin >> H;
     result = calculatePoolState(V, P1, P2, H);
     cout << result;
+}
+
+string calculatePoolState(int volume, int pipe1, int pipe2, float hours)
+{
+
+    float totalFlow;
+    int fillPerc, totalP1Flow, totalP2Flow, p1Perc, p2Perc;
+    totalP1Flow = pipe1 * hours;
+    totalP2Flow = pipe2 * hours;
+    totalFlow = totalP1Flow + totalP2Flow;
+    if (totalFlow > volume)
+    {
+        int overflow = totalFlow - volume;
+        stringstream stream;
+        stream << fixed << setprecision(1) << hours;
+        return "For " + stream.str() + " hours, the pool overflows with " + to_string(overflow) + " liters.";
+    }
+    if (totalFlow <= volume)
+    {
+        fillPerc = (totalFlow / volume) * 100;
+        p1Perc = (totalP1Flow / totalFlow) * 100;
+        p2Perc = (totalP2Flow / totalFlow) * 100;
+        return "The pool is " + to_string(fillPerc) + "% full. Pipe 1: " + to_string(p1Perc) + "%. Pipe 2: " + to_string(p2Perc) + "%.";
+    }
 }
