5d420f893e31b9ca4c4c8306c2ee601f260dbcbb
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-10-10 13:57:18 +0500

    Complete Task05

---
+++ Week6/Task05OP.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+float discount(string,string,float);
+main(){
+    string day,month;
+    float amount;
+    cout<<"Enter Purchase Day: ";
+    cin>>day;
+    cout<<"Enter Purchase Month: ";
+    cin>>month;
+    cout<<"Enter Purchase Amount: ";
+    cin>>amount;
+    float payable = discount(day,month,amount);
+    cout<<"Payable Amount after discount: "<<payable;
+}
+
+float discount(string day,string month,float amount){
+    if(day == "Sunday" && month == "October"){
+        return amount - amount * 0.1;
+    }
+    if(day == "Sunday" && month != "October"){
+        return amount - amount * 0.05;
+    }
+    return amount;
+}
