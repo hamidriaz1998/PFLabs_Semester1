0643457b451f7d3bc297f5ce96c91e69713752e9
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-10-10 13:50:09 +0500

    Complete Task04

---
+++ Week6/Task04CL.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int findGreatest(int, int, int);
+main()
+{
+    int n1, n2, n3;
+    cout << "Enter the first number: ";
+    cin >> n1;
+    cout << "Enter the second number: ";
+    cin >> n2;
+    cout << "Enter the third number: ";
+    cin >> n3;
+    int result = findGreatest(n1, n2, n3);
+    cout << "The greatest number among " << n1 << ", " << n2 << ", and " << n3 << " is: " << result;
+}
+
+int findGreatest(int n1, int n2, int n3)
+{
+    if (n1 > n2 && n1 > n3)
+    {
+        return n1;
+    }
+    if (n2 > n1 && n2 > n3)
+    {
+        return n2;
+    }
+    if (n3 > n1 && n3 > n2)
+    {
+        return n3;
+    }
+}
