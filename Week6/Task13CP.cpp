3c988c9e277f9cfc40587e936177f9ae872b49d1
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-10-10 14:39:11 +0500

    Start Task13

---
+++ Week6/Task13CP.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+float lowestPrice(int, string);
+main(){
+    int kilometers;
+    string period;
+    cout<<"Enter the number of kilometers: ";
+    cin>>kilometers;
+    cout<<"Enter the period of the day (day/night): ";
+    cin>>period;
+    float result = lowestPrice(kilometers,period);
+    cout<<"Lowest price for "<<kilometers<<" kilometers: "<<result<<" EUR";
+}
+
+float lowestPrice(int kilometers, string period){
+    if (kilometers < 20){
+        if (period == "day"){
+            return kilometers * 0.79;
+        }
+        if (period == "night"){
+            return kilometers * 0.90;
+        }
+    }
+    if (kilometers >= 20 && kilometers < 100){
+        return kilometers * 0.09;
+    }
+    if (kilometers>= 100 ){
+        return kilometers *0.06;
+    }
+}
