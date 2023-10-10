3aa904d470c2a13c9ebd74be4a76d2f070576d8d
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-10-10 15:02:41 +0500

    Complete Task14

--- Week6/Task14CP.cpp
+++ Week6/Task14CP.cpp
@@ -1,19 +1,58 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 string calculateCost(float budget, string category, int numPeople);
-main(){
+main()
+{
     float budget;
     int numPeople;
     string category;
-    cout<<"Enter the budget: ";
-    cin>>budget;
-    cout<<"Enter the category (VIP/Normal): ";
-    cin>>category;
-    cout<<"Enter the number of people in the group: ";
-    cin>>numPeople;
-    cout<<calculateCost(budget,category,numPeople);
+    cout << "Enter the budget: ";
+    cin >> budget;
+    cout << "Enter the category (VIP/Normal): ";
+    cin >> category;
+    cout << "Enter the number of people in the group: ";
+    cin >> numPeople;
+    cout << calculateCost(budget, category, numPeople);
 }
 
-string calculateCost(float budget, string category, int numPeople){
-    
+string calculateCost(float budget, string category, int numPeople)
+{
+    float ticketCost = 0, transportCost = 0;
+    if (category == "VIP")
+    {
+        ticketCost = 499.99 * numPeople;
+    }
+    if (category == "Normal")
+    {
+        ticketCost = 249.99 * numPeople;
+    }
+    if (numPeople >= 1 && numPeople <= 4)
+    {
+        transportCost = budget * 0.75;
+    }
+    if (numPeople >= 5 && numPeople <= 9)
+    {
+        transportCost = budget * 0.60;
+    }
+    if (numPeople >= 10 && numPeople <= 24)
+    {
+        transportCost = budget * 0.50;
+    }
+    if (numPeople >= 25 && numPeople <= 49)
+    {
+        transportCost = budget * 0.40;
+    }
+    if (numPeople >= 50)
+    {
+        transportCost = budget * 0.25;
+    }
+    float remaining = budget - transportCost;
+    if (remaining >= ticketCost){
+        remaining -= ticketCost;
+        return "Yes! You have " + to_string(remaining) + " leva left.";
+    }
+    if (remaining < ticketCost){
+        remaining = ticketCost - remaining;
+        return "Not enough money! You need " + to_string(remaining) + " leva.";
+    }
 }
