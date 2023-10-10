e9d519ad757e3677cb42ad205caf695de4158c5c
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-10-10 14:12:36 +0500

    Complete Task09

---
+++ Week6/Task09CL.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+string checkTitle(int, char);
+main()
+{
+    float age;
+    char gender;
+    cout << "Enter your age: ";
+    cin >> age;
+    cout << "Enter your gender (m/f): ";
+    cin >> gender;
+    string result = checkTitle(age, gender);
+    cout << "Your personal title is: " << result;
+}
+
+string checkTitle(int age, char gender)
+{
+    if (age >= 16 && gender == 'm')
+    {
+        return "Mr.";
+    }
+    if (age < 16 && gender == 'm')
+    {
+        return "Master";
+    }
+    if (age >= 16 && gender == 'f')
+    {
+        return "Ms.";
+    }
+    if (age < 16 && gender == 'f')
+    {
+        return "Miss";
+    }
+}
