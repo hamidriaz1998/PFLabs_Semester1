63afa6cfa824cc78d3283f43147e8f9e2c76dd71
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-10-10 14:48:14 +0500

    Complete Task13

--- Week6/Task13CP.cpp
+++ Week6/Task13CP.cpp
@@ -1,30 +1,37 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 float lowestPrice(int, string);
-main(){
+main()
+{
     int kilometers;
     string period;
-    cout<<"Enter the number of kilometers: ";
-    cin>>kilometers;
-    cout<<"Enter the period of the day (day/night): ";
-    cin>>period;
-    float result = lowestPrice(kilometers,period);
-    cout<<"Lowest price for "<<kilometers<<" kilometers: "<<result<<" EUR";
+    cout << "Enter the number of kilometers: ";
+    cin >> kilometers;
+    cout << "Enter the period of the day (day/night): ";
+    cin >> period;
+    float result = lowestPrice(kilometers, period);
+    cout << "Lowest price for " << kilometers << " kilometers: " << result << " EUR";
 }
 
-float lowestPrice(int kilometers, string period){
-    if (kilometers < 20){
-        if (period == "day"){
-            return kilometers * 0.79;
+float lowestPrice(int kilometers, string period)
+{
+    if (kilometers < 20)
+    {
+        if (period == "day")
+        {
+            return (kilometers * 0.79) + 0.70;
         }
-        if (period == "night"){
-            return kilometers * 0.90;
+        if (period == "night")
+        {
+            return (kilometers * 0.90) + 0.70;
         }
     }
-    if (kilometers >= 20 && kilometers < 100){
+    if (kilometers >= 20 && kilometers < 100)
+    {
         return kilometers * 0.09;
     }
-    if (kilometers>= 100 ){
-        return kilometers *0.06;
+    if (kilometers >= 100)
+    {
+        return kilometers * 0.06;
     }
 }
