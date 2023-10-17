c2d697a429b98176c68fafcf4d3be77bf48198fd
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-10-17 07:58:16 +0500

    Complete Task3

--- Week7/Task3CP.cpp
+++ Week7/Task3CP.cpp
@@ -1,21 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int totalDigits(int);
-main(){
+main()
+{
     int n;
-    cout<<"Enter a number: ";
-    cin>>n;
+    cout << "Enter a number: ";
+    cin >> n;
     int result = totalDigits(n);
-    cout<<"Total number of digits: "<<result;
+    cout << "Total number of digits: " << result;
 }
 
-int totalDigits(int number){
+int totalDigits(int number)
+{
     int DigitsInNumber = 0;
-    if (number >= 0 && number <=9){
+    if (number >= 0 && number <= 9)
+    {
         return 1;
     }
-    while (number != 0){
-        number = number/ 10;
+    while (number != 0)
+    {
+        number = number / 10;
         DigitsInNumber++;
     }
     return DigitsInNumber;
