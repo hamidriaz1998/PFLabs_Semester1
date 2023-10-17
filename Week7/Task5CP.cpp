44203015d87a0ef2aeb5271953eef20e2167fcc0
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-10-17 08:22:58 +0500

    Complete Task5

--- Week7/Task5CP.cpp
+++ Week7/Task5CP.cpp
@@ -1,20 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int digitSum(int number);
-main(){
+main()
+{
     int n;
-    cout<<"Enter a number: ";
-    cin>>n;
+    cout << "Enter a number: ";
+    cin >> n;
     int sum = digitSum(n);
-    cout<<"Sum of digits: "<<sum;
+    cout << "Sum of digits: " << sum;
 }
 
-int digitSum(int number){
-    int Obtained_digit = 0,sum=0;
-    while (number != 0){
-        Obtained_digit = number %10;
-        number = number /10;
+int digitSum(int number)
+{
+    int Obtained_digit = 0, sum = 0;
+    while (number != 0)
+    {
+        Obtained_digit = number % 10;
+        number = number / 10;
         sum += Obtained_digit;
     }
     return sum;
-}   
+}
