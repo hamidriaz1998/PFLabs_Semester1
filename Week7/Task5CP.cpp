32a05a187c313ceed5d4cf1e91f7ece3da4bc405
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-10-17 08:22:47 +0500

    Write DigitSum Function

--- Week7/Task5CP.cpp
+++ Week7/Task5CP.cpp
@@ -9,3 +9,12 @@ main(){
     cout<<"Sum of digits: "<<sum;
 }
 
+int digitSum(int number){
+    int Obtained_digit = 0,sum=0;
+    while (number != 0){
+        Obtained_digit = number %10;
+        number = number /10;
+        sum += Obtained_digit;
+    }
+    return sum;
+}   
