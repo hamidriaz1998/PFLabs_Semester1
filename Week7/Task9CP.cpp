445eb3a086b7059533c8078ef589bf897bf58531
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-10-17 08:38:29 +0500

    Write Calculate Price Function

--- Week7/Task9CP.cpp
+++ Week7/Task9CP.cpp
@@ -10,3 +10,19 @@ main(){
     int result = calculatePrice(money,year);
     
 }
+
+int calculatePrice(int money, int year){
+    int price =0;
+    int age = 18;
+    for (int i = 1800; i<=year;i++){
+        if (i % 2 == 0){
+            price += 12000;
+        }
+        else{
+            i += 12000 + 50*age;
+        }
+        age++;
+    }
+    money = money - price;
+    return money;
+}
