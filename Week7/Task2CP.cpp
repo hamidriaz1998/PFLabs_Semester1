7f40120676c601139e63b586dc20290659449981
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-10-17 07:37:57 +0500

    Complete main function

---
+++ Week7/Task2CP.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+void generateFibonacci(int length);
+main(){
+    int n;
+    cout<<"Enter the length of the Fibonacci series: ";
+    cin>>n;
+    generateFibonacci(n);
+}
