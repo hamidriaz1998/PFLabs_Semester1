0de639ea7851d636f903c12fda00294d2684e4d7
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-10-17 07:57:29 +0500

    Complete TotalDigits Function

--- Week7/Task3CP.cpp
+++ Week7/Task3CP.cpp
@@ -8,3 +8,15 @@ main(){
     int result = totalDigits(n);
     cout<<"Total number of digits: "<<result;
 }
+
+int totalDigits(int number){
+    int DigitsInNumber = 0;
+    if (number >= 0 && number <=9){
+        return 1;
+    }
+    while (number != 0){
+        number = number/ 10;
+        DigitsInNumber++;
+    }
+    return DigitsInNumber;
+}
