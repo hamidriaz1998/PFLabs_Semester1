d037e323b3ef882d1c814c552653181469dfa766
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-10-17 08:50:04 +0500

    Complete Task9

--- Week7/Task9CP.cpp
+++ Week7/Task9CP.cpp
@@ -1,30 +1,37 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int calculatePrice(int money, int year);
-main(){
-    int money,year;
-    cout<<"Enter Money: ";
-    cin>>money;
-    cout<<"Enter Year: ";
-    cin>>year;
-    int result = calculatePrice(money,year);
-        if (result >=0){
-        cout<<"Yes! He will live a carefree life and will have "<<result<<" dollars left.";
+main()
+{
+    int money, year;
+    cout << "Enter Money: ";
+    cin >> money;
+    cout << "Enter Year: ";
+    cin >> year;
+    int result = calculatePrice(money, year);
+    if (result >= 0)
+    {
+        cout << "Yes! He will live a carefree life and will have " << result << " dollars left.";
     }
-    else{
-        cout<<"He will need "<<abs(result)<<" dollars to survive.";
+    else
+    {
+        cout << "He will need " << abs(result) << " dollars to survive.";
     }
 }
 
-int calculatePrice(int money, int year){
-    int price =0;
+int calculatePrice(int money, int year)
+{
+    int price = 0;
     int age = 18;
-    for (int i = 1800; i<=year;i++){
-        if (i % 2 == 0){
+    for (int i = 1800; i <= year; i++)
+    {
+        if (i % 2 == 0)
+        {
             price += 12000;
         }
-        else{
-            price += 12000 + 50*age;
+        else
+        {
+            price += 12000 + 50 * age;
         }
         age++;
     }
