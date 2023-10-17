1bfdc24088e1911d7eff29ba694063fe6cb54d11
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-10-17 07:43:32 +0500

    Complete Fibonnaci Function

--- Week7/Task2CP.cpp
+++ Week7/Task2CP.cpp
@@ -7,3 +7,17 @@ main(){
     cin>>n;
     generateFibonacci(n);
 }
+
+void generateFibonacci(int length){
+    int a = 0,b=1, Next_Term;
+    for (int i = 0;i< length;i++){
+        cout<<a;
+        if (i != length -1){
+            cout<<", ";
+        }
+        Next_Term = a+ b;
+        a = b;
+        b = Next_Term;
+
+    }
+}
