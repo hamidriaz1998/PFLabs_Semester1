44bcc6cdc5d2b269f3d0af2a348fd423522ba638
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-10-17 13:35:58 +0500

    Complete Main function

--- Week7/Task6CP.cpp
+++ Week7/Task6CP.cpp
@@ -3,5 +3,11 @@ using namespace std;
 int calculateGCD(int number1, int number2);
 int calculateLCM(int number1,int number2);
 main(){
-    
+    int n1,n2;
+    cout<<"Enter the first number: ";
+    cin>>n1;
+    cout<<"Enter the second number: ";
+    cin>>n2;
+    cout<<"GCD: "<<calculateGCD(n1,n2);
+    cout<<"LCM: "<<calculateLCM(n1,n2);
 }
