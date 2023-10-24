20b2ac28735b1e741194c86a038a6b305d6fcada
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-10-24 13:50:41 +0500

    Complete Task2

--- Week8/Task2.cpp
+++ Week8/Task2.cpp
@@ -7,3 +7,27 @@ main()
     cout << calculateDamage("grass", "fire", 35, 5) << endl;
     cout << calculateDamage("electric", "fire", 100, 100);
 }
+
+int calculateDamage(string playerType, string opponentType, int attackPower, int defensePower)
+{
+    float effectivness;
+    // Not very effective
+    if ((playerType == opponentType)                                                          // Same type condition
+        || (playerType == "fire" && opponentType == "water")                                  // fire < water condition
+        || (playerType == "water" && (opponentType == "grass" || opponentType == "electric")) // water < grass,electric
+        || (playerType == "grass" && opponentType == "fire")                                  // inverse of fire > grass
+    )
+    {
+        effectivness = 0.5;
+    }
+    // Neutral
+    else if ((playerType == "fire" && opponentType == "electric") || (playerType == "grass" && opponentType == "electric") || (playerType == "electric" && opponentType == "fire") || (playerType == "electric" && opponentType == "grass"))
+    {
+        effectivness = 1;
+    }
+    else
+    {
+        effectivness = 2;
+    }
+    return 50 * (attackPower / defensePower) * effectivness;
+}
