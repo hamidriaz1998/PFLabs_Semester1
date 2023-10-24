d0114843f06be8de04b7e397dc09decf4dc04159
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-10-24 13:54:13 +0500

    Write main function

---
+++ Week8/Task3.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+string plant(string,int,int,int);
+main(){
+    cout<<plant("@", 3, 3, 25) <<endl;
+    cout<<plant("#", 1, 5, 30)<<endl;
+    cout<<plant("&", 5, 1, 20)<<endl;
+    cout<<plant("§", 3, 3, 15)<<endl;
+}
