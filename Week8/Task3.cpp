db10b139e2d5016c243345a5a9277d5501b1cf4d
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-10-24 14:25:36 +0500

    Fix plant Function

--- Week8/Task3.cpp
+++ Week8/Task3.cpp
@@ -1,29 +1,41 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-string plant(string,int,int,int);
-// void printFlowers(string,int);
-main(){
-    cout<<plant("@", 3, 3, 25) <<endl;
-    cout<<plant("#", 1, 5, 30)<<endl;
-    cout<<plant("&", 5, 1, 20)<<endl;
-    cout<<plant("§", 3, 3, 15)<<endl;
+string plant(string, int, int, int);
+main()
+{
+    cout << plant("@", 3, 3, 25) << endl;
+    cout << plant("#", 1, 5, 30) << endl;
+    cout << plant("&", 5, 1, 20) << endl;
+    cout << plant("§", 3, 3, 15) << endl;
 }
 
-string plant(string seed,int water,int fert,int temp){
+string plant(string seed, int water, int fert, int temp)
+{
     string plant1 = "";
     int count = water;
-    while (count != 0){
-    for (int i = 1;i<=water;i++){
-        plant1 +="-";
+    if (temp >= 20 && temp <= 30)
+    {
+        while (count != 0)
+        {
+            for (int i = 1; i <= water; i++)
+            {
+                plant1 += "-";
+            }
+            for (int j = 1; j <= fert; j++)
+            {
+                plant1 += seed;
+            }
+            count--;
+        }
+    
     }
-    for (int j = 1; j<= fert; j++){
-        plant1 += seed;
+    else{
+        for(int i =1;i<=water;i++){
+            for(int j =1;j<=water;j++){
+                plant1 += "-";
+            }
+        }
+        plant1+= seed;
     }
-    count--;
-    }
-    // printFlowers(seed,fert);
     return plant1;
 }
-
-// void printFlowers(string seed,int fert){
-// }
