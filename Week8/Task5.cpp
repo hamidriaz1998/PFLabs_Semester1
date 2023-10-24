32a30b133997dd7b18024d94c190772a80ffa1cd
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-10-24 14:30:15 +0500

    Start Task5

---
+++ Week8/Task5.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+string howBad(int);
+int toBinary(int);
+int digitSum(int);
+main(){
+    cout<<howBad(7)<<endl;
+    cout<<howBad(17)<<endl;
+    cout<<howBad(23)<<endl;
+}
