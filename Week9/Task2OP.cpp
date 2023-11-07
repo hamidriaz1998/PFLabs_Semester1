b46e1d63b7cdf4376e22a11c24741de69b51e269
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-11-07 13:49:58 +0500

    Complete Task2

--- Week9/Task2OP.cpp
+++ Week9/Task2OP.cpp
@@ -1,5 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-main(){
-    
+main()
+{
+    string word;
+    cout << "Enter a string: ";
+    cin >> word;
+    int count = 0;
+    for (int i = 0; word[i] != '\0'; i++)
+        count++;
+    cout << "Reversed String: ";
+    for (int i = count; i >= 0; i--)
+        cout << word[i];
 }
