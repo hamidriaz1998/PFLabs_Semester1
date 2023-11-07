f88123081083dc81ff727b09f35f3e0f593d9719
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-11-07 13:36:22 +0500

    Start Task5

--- Week8/Task5.cpp
+++ Week8/Task5.cpp
@@ -1,10 +1,28 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 string howBad(int);
 int toBinary(int);
 int digitSum(int);
-main(){
-    cout<<howBad(7)<<endl;
-    cout<<howBad(17)<<endl;
-    cout<<howBad(23)<<endl;
+main()
+{
+    cout << howBad(7) << endl;
+    cout << howBad(17) << endl;
+    cout << howBad(23) << endl;
+}
+
+int digitSum(int n){
+    int sum =0;
+    while (n!=0){
+        sum += n%10;
+        n /= 10;
+    }
+    return sum;
+}
+
+int toBinary(int n){
+    string num = "";
+    while(n!=0){
+        num += to_string(n/2);
+        n /= 2;
+    }
 }
