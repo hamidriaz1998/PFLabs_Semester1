9f103d763876645cdbf3a569edeffd0cce388f64
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-11-07 20:43:37 +0500

    Complete Task13

--- Week9/Task13CP.cpp
+++ Week9/Task13CP.cpp
@@ -2,5 +2,34 @@
 using namespace std;
 void jazzifyChords(string chords[],int numChords);
 main(){
-    
+        int numChords;
+    cout << "Enter the number of chords: ";
+    cin >> numChords;
+    cout << "Enter " << numChords << " chords, one per line: "<<endl;
+    string arr[numChords];
+    for (int i = 0; i < numChords; i++)
+    {
+        cin >> arr[i];
+    }
+    jazzifyChords(arr,numChords);
+}
+
+void jazzifyChords(string chords[],int numChords){
+    for (int i = 0;i<numChords;i++){
+        int size = 0;
+        for (int n = 0;chords[i][n]!='\0';n++){
+            size++;
+        }
+        if (chords[i][size-1] != '7'){
+            chords[i] += '7';
+        }
+    }
+    cout<<"Jazzified chords: [";
+    for(int i = 0;i<numChords;i++){
+        cout<<chords[i];
+        if (i != numChords-1){
+            cout<<", ";
+        }
+    }
+    cout<<"]";
 }
