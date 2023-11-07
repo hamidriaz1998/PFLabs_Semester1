004425bc8903aba584486e87784ca4568b0768a7
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-11-07 13:36:00 +0500

    Complete Task1

--- Week9/Task1OP.cpp
+++ Week9/Task1OP.cpp
@@ -1,5 +1,10 @@
 #include<iostream>
 using namespace std;
 main(){
-    
+    string word;
+    cout<<"Enter a word: ";
+    cin>>word;
+    for (int i=0;word[i]!='\0';i++){
+        cout<<word[i]<<" found at position "<<i<<endl;
+    }
 }
