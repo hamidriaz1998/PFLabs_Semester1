13bc331f1f8a4c273e3c6ba4ff7f12d055cd60d9
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-11-28 13:27:11 +0500

    Complete Task4OP

---
+++ Week12/Task4OP.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int sum(int arr[][3], int size);
+main()
+{
+    int arr[3][3];
+    int size;
+    cout << "Enter row size: ";
+    cin >> size;
+    cout << "Enter the elements of the matrix: ";
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            cout << "Enter the element at position [" << i << "][" << j << "]: ";
+            cin >> arr[i][j];
+        }
+    }
+    cout << "The sum of elements in the matrix is: " << sum(arr,size);
+}
+int sum(int arr[][3],int size)
+{
+    int result = 0;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            result += arr[i][j];
+        }
+    }
+    return result;
+}
